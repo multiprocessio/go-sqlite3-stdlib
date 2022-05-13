f9470826bd017f753d192692ddb739649f1efe4b
Phil Eaton <phil@eatonphil.com>
2022-05-13 16:58:34 +0000

    Fix mac/windows builds

--- scripts/ci/prepare_macos.sh
+++ scripts/ci/prepare_macos.sh
@@ -2,6 +2,10 @@
 
 set -eux
 
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew uninstall go@1.15
-brew install go jq
+GOVERSION=1.18.2
+
+sudo curl -LO https://go.dev/dl/go$GOVERSION.darwin-amd64.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf go$GOVERSION.darwin-amd64.tar.gz
+sudo mv /usr/local/go/bin/go /usr/local/bin/go
+sudo mv /usr/local/go/bin/gofmt /usr/local/bin/gofmt
